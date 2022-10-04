e0fed7f9c49cd158998115718ecc0262935e9fa9
NueveCuatro <coleelliot2001@gmail.com>
2022-10-04 16:27:02 +0200

    premiere essaie

--- src/Controller/GarageController.php
+++ src/Controller/GarageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Garage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class GarageController extends AbstractController
 {
@@ -15,4 +17,40 @@ class GarageController extends AbstractController
             'controller_name' => 'GarageController',
         ]);
     }
+
+    /**
+     * liste des garages
+     * 
+     * @route("/liste, name = 'Garage_list, methods = "GET") 
+     */
+    public function listGarage(ManagerRegistry $doctrine)
+    {
+        $htmlpage = '<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>garages list!</title>
+    </head>
+    <body>
+        <h1>garages list</h1>
+        <p>Here are all your garages:</p>
+        <ul>';
+        
+        $entityManager= $doctrine->getManager();
+        $garages = $entityManager->getRepository(Garage::class)->findAll();
+        foreach($garages as $garage) {
+           $htmlpage .= '<li>
+            <a href="/garage/'.$garage->getid().'">'.$garage->getTitle().'</a></li>';
+         }
+        $htmlpage .= '</ul>';
+
+        $htmlpage .= '</body></html>';
+        
+        return new Response(
+            $htmlpage,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+            );
+    }
+
 }
